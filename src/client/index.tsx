e241b02de0c2d78af609a2571382e514cd20e7f0
Rimaz Khan <rimazk@umich.edu>
2021-05-06 18:57:23 -0400

    update scripts

--- src/client/index.tsx
+++ src/client/index.tsx
@@ -3,7 +3,7 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import App from './components/App';
 
-console.log('Hello from tsx!')
+console.log('Hello from React')
 
 ReactDOM.render(
   <App />,
--- src/server/server.ts
+++ src/server/server.ts
@@ -2,14 +2,12 @@ import express from 'express';
 import { graphqlHTTP } from 'express-graphql'
 import { schema } from './schema'
 import { context } from './context'
-const fs = require('fs')
-const path = require('path')
 
 const app = express();
 const Bundler = require('parcel-bundler');
 
 const file = 'src/client/index.html';
-const options = {}; // See options section of api docs, for the possibilities
+const options = { production: process.env.NODE_ENV === 'production' }; 
 
 const bundler = new Bundler(file, options);
 
@@ -22,7 +20,6 @@ app.use(
   }),
 )
 app.use(bundler.middleware());
-
 
 app.listen(4000)
 console.log(`\
